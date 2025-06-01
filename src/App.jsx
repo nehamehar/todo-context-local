b1eeb03cc7daa3b7db9faed28a04e641afd7320b
Neha Mehar <nehamehar31@gmail.com>
2025-06-01 21:58:49 +0530

    added %

--- src/App.jsx
+++ src/App.jsx
@@ -75,11 +75,11 @@ const todoToDelete = todos.find(todo => todo.id === id);
   return (
     <TodoProvider value={{todos, addTodo, updatedTodo, deletedTodo, toggleCompleted}}>    {/* whenever we use provider we have to mention what provider will give like value and all */}
     
-    <div className="flex flex-col min-h-screen bg-center bg-no-repeat bg-cover "style={{
+    <div className="flex flex-col min-h-screen bg-center bg-no-repeat bg-cover"style={{
     backgroundImage: "url('https://img.pikbest.com/backgrounds/20220119/vintage-texture-kraft-paper-brown-retro_6237865.jpg!sw800')"
   }}>
                 <div className="flex-grow w-full max-w-2xl px-4 py-6 mx-auto text-black rounded-lg shadow-md">
-                    <h1 className="mt-2 text-4xl font-extrabold text-center text-slate-800 mb-9">TO-DO App</h1>
+                    <h1 className="mt-2 text-4xl font-extrabold text-center text-slate-800 mb-9 sm:text-6xl ">TO-DO App</h1>
                     <div className="mb-10">
                         {/* Todo form goes here */} 
                         <TodoForm/>
@@ -117,8 +117,7 @@ const todoToDelete = todos.find(todo => todo.id === id);
 
                     
                 </div>
-                <footer className="flex items-center justify-between w-full px-3 py-5 text-base text-gray-500 bg-neutral-800 sm:px-10 ">
-
+                <footer className="flex items-center justify-between w-full px-3 py-5 text-gray-500 md:text-base bg-neutral-800">
       <p className='ml-36'>
         Made with <span className="text-lg text-red-500 animate-pulse">❤️</span> by {""}
         <a
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -16,12 +16,12 @@ function TodoForm() {
             <input
                 type="text"
                 placeholder="Write Todo..."
-                className="w-full px-4 py-1.5 duration-150 rounded-l-lg outline-none border-gray-800 bg-black/10 border-5 border backdrop-filter backdrop-brightness-40"
+                className="w-full px-4 py-1.5 duration-150 rounded-l-lg outline-none border-gray-800 bg-black/10 border-5 border backdrop-filter backdrop-brightness-40 text-sm sm:text-base sm:px-7 sm:py-2"
                 value={todo}  // for adding input with date we use value ={todo} the value is binding input to todo
                 onChange={(e) => setTodo(e.target.value)}
                 // e.target.value (e.target = shows input fleid, e.target.value= shows input current value which we have give to input (value={todo}) )
             />
-            <button type="submit" className="px-3 py-2 text-black bg-green-600 border rounded-r-lg shrink-0 border-slate-800">
+            <button type="submit" className="px-2 px-3 py-1 py-2 text-xs text-black bg-green-600 border rounded-r-lg shrink-0 border-slate-800 sm:text-sm sm:px-4 sm:py-2">
                 Add
             </button>
         </form>
